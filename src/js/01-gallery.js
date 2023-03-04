3e7deeb3c2c05a9a8938b2bf5991e024d50cfb0d
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-03-04 16:32:57 +0100

    update Task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryArrRef = document.querySelector('.gallery');
+const picturesMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>`
+  )
+  .join('');
+
+galleryArrRef.insertAdjacentHTML('afterbegin', picturesMarkup);
+
+// SimpleLightbox library use
+
+new SimpleLightbox('.gallery a', {
+  captionType: 'alt',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
