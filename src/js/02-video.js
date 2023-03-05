aa5555c11456999e4978dc04849fa693b246689e
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-03-05 11:53:49 +0100

    Task-02 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALST_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const seconds = data.seconds;
+  localStorage.setItem(LOCALST_KEY, JSON.stringify(seconds));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(LOCALST_KEY));
+player.setCurrentTime(currentTime);
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
