adfaa4a17cbfd8cc52d4208f2323008ff5e62fee
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-03-05 20:22:03 +0100

    Task-03 finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALST_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(savedData, 500));
+function savedData(event) {
+  event.preventDefault();
+  const dataToSave = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALST_KEY, JSON.stringify(dataToSave));
+}
+
+updateForm();
+function updateForm() {
+  const parsedData = JSON.parse(localStorage.getItem(LOCALST_KEY)) || '';
+  if (parsedData || '') {
+    email.value = parsedData.email;
+    message.value = parsedData.message;
+  }
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log({ Email: email.value, Message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCALST_KEY);
+});
+
+// const test = throttle(() => console.log('blablatest'), 3000);
+// test();
+
+// console.log(email);
+// console.log(message);
+
+// const savedData = localStorage.getItem(LOCALST_KEY);
+// const parsedData = JSON.parse(savedData) || "";
+//   console.log(parsedData);
